6bc764de72869138292d37d1715124ac2b135be6
jonnigs <jonnigs@gmail.com>
2020-03-26 14:07:39 +0000

    SVG Landið
    Kominn með kort af Íslandi sem breytir um lit eftir landshlutum þegar mús hover-ar.

--- framendi/src/routes/Map.js
+++ framendi/src/routes/Map.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, Component } from "react";
 import WebGL from "../components/WebGL";
+import Iceland from "../components/Iceland";
 import Sudodata from "../sudodata.json";
 
 console.log(Sudodata.results);
@@ -8,7 +9,7 @@ class Map extends Component {
   render() {
     return (
       <main className="main">
-        <WebGL />
+        <Iceland />
       </main>
     );
   }
