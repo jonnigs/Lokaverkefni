62b8bc031388f87b6f55f82e83a98b37b99a34b9
jonnigs <jonnigs@gmail.com>
2020-03-27 17:07:05 +0000

    *

--- framendi/src/routes/Map.js
+++ framendi/src/routes/Map.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, Component } from "react";
-import WebGL from "../components/WebGL";
 import Iceland from "../components/Iceland";
 import Sudodata from "../sudodata.json";
 import ClimbingAreaData from "../climbingAreaData.json";
@@ -7,12 +6,42 @@ import ClimbingAreaData from "../climbingAreaData.json";
 console.log(Sudodata.results);
 
 class Map extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      loading: true,
+      id: this.props.match.params.id
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      this.setState({ id: this.props.match.params.id, loading: false });
+    } catch (error) {
+      console.error("Error fetching books", error);
+      this.setState({ error: true, loading: false });
+    }
+  }
+
   render() {
+    const { loading, error, id } = this.state;
+
+    const landshluti = ClimbingAreaData.results.map(t => {
+      if (id == t.id) {
+        return <h1>{t.nafn}</h1>;
+      }
+    });
+
+    const svaedi = ClimbingAreaData.results[0].svaedi.map(svaedi => (
+      <p>{svaedi}</p>
+    ));
+
     return (
       <main className="main">
         <Iceland />
-        <p>{this.props.match.params.id}</p>
-        <p>{ClimbingAreaData.results[0].nafn}</p>
+        {landshluti}
+        {svaedi}
       </main>
     );
   }
