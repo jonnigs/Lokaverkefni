429ad8c5ce52311c8c26490ba903ca82da48181e
jonnigs <jonnigs@gmail.com>
2020-04-16 23:15:59 +0000

    Fullsrceen takki

--- model/scripts.js
+++ model/scripts.js
@@ -4,23 +4,23 @@ const model = document.getElementsByTagName("CANVAS")[0];
 
 model.classList.add("model");
 
-console.log(fullscreen);
-
-fullscreen.addEventListener("click", () => {
+document.addEventListener("click", e => {
   const elem = document.getElementsByClassName("model");
 
-  if (elem.requestFullscreen) {
-    elem.requestFullscreen();
-  } else if (elem.mozRequestFullScreen) {
-    /* Firefox */
-    elem.mozRequestFullScreen();
-  } else if (elem.webkitRequestFullscreen) {
-    /* Chrome, Safari and Opera */
-    console.log("her");
+  console.log(e);
 
-    elem.webkitRequestFullscreen();
-  } else if (elem.msRequestFullscreen) {
-    /* IE/Edge */
-    elem.msRequestFullscreen();
-  }
+  //if (elem.requestFullscreen) {
+  //elem.requestFullscreen();
+  //} else if (elem.mozRequestFullScreen) {
+  /* Firefox */
+  //elem.mozRequestFullScreen();
+  //} else if (elem.webkitRequestFullscreen) {
+  /* Chrome, Safari and Opera */
+  //console.log("her");
+
+  //elem.webkitRequestFullscreen();
+  //} else if (elem.msRequestFullscreen) {
+  /* IE/Edge */
+  //elem.msRequestFullscreen();
+  //}
 });
