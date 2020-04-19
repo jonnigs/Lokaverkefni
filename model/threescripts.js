9601412e59805917c4e094e21415d3b083ff0a54
jonnigs <jonnigs@gmail.com>
2020-04-19 00:37:58 +0000

    Sýna og fela virkar!

--- model/threescripts.js
+++ model/threescripts.js
@@ -1,5 +1,10 @@
 var BaseURL =
   "https://notendur.hi.is/~jgs7/Lokaverkefni/modelFiles/mulafjall/e-kaffistofan/";
+var OBJnumbers;
+var numbersVisable = true;
+var OBJroutes;
+var routesVisable = true;
+var OBJmodel;
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(
   75,
@@ -28,21 +33,49 @@ controls.enableZoom = true;
 // Hlaða númerum
 var mtlLoader = new THREE.MTLLoader();
 mtlLoader.setTexturePath(BaseURL);
-mtlLoader.setPath(
-  "https://notendur.hi.is/~jgs7/Lokaverkefni/modelFiles/mulafjall/e-kaffistofan/"
-);
-mtlLoader.load("kaffistofanCutOff.mtl", function (materials) {
+mtlLoader.setPath(BaseURL);
+mtlLoader.load("baraNumer/untitled.mtl", function (materials) {
   materials.preload();
   var objLoader = new THREE.OBJLoader();
   objLoader.setMaterials(materials);
-  objLoader.setPath(
-    "https://notendur.hi.is/~jgs7/Lokaverkefni/modelFiles/mulafjall/e-kaffistofan/"
-  );
-  objLoader.load("kaffistofanCutOff.obj", function (object) {
+  objLoader.setPath(BaseURL);
+  objLoader.load("baraNumer/untitled.obj", function (object) {
+    OBJnumbers = object;
     scene.add(object);
   });
 });
-var geometry = new THREE.BoxGeometry(1, 1, 1);
+
+// Hlaða leiðum
+var mtlLoader2 = new THREE.MTLLoader();
+mtlLoader2.setTexturePath(BaseURL);
+mtlLoader2.setPath(BaseURL);
+mtlLoader2.load("baraLeidir/untitled.mtl", function (materials) {
+  materials.preload();
+  var objLoader2 = new THREE.OBJLoader();
+  objLoader2.setMaterials(materials);
+  objLoader2.setPath(BaseURL);
+  objLoader2.load("baraLeidir/untitled.obj", function (object) {
+    OBJroutes = object;
+    scene.add(object);
+  });
+});
+
+// Hlaða módeli
+var mtlLoader3 = new THREE.MTLLoader();
+mtlLoader3.setTexturePath(BaseURL);
+mtlLoader3.setPath(BaseURL);
+mtlLoader3.load("baraModel/untitled.mtl", function (materials) {
+  materials.preload();
+  var objLoader3 = new THREE.OBJLoader();
+  objLoader3.setMaterials(materials);
+  objLoader3.setPath(BaseURL);
+  objLoader3.load("baraModel/untitled.obj", function (object) {
+    OBJmodel = object;
+    scene.add(object);
+  });
+});
+
+/*var geometry = new THREE.BoxGeometry(1, 1, 1);
 var material = new THREE.MeshLambertMaterial({ color: 0xddd });
 var mesh = new THREE.Mesh(geometry, material);
 var meshVisable = true;
@@ -56,7 +89,7 @@ mesh2.position.y = -1;
 mesh2.position.z = -1;
 
 scene.add(mesh);
-scene.add(mesh2);
+scene.add(mesh2);*/
 
 window.addEventListener("resize", () => {
   renderer.setSize(window.innerWidth, window.innerHeight);
@@ -69,21 +102,21 @@ const numbers = document.getElementById("labelVisability");
 const routes = document.getElementById("routeVisability");
 
 numbers.addEventListener("click", (e) => {
-  if (meshVisable) {
-    scene.remove(mesh);
+  if (numbersVisable) {
+    scene.remove(OBJnumbers);
   } else {
-    scene.add(mesh);
+    scene.add(OBJnumbers);
   }
-  meshVisable = !meshVisable;
+  numbersVisable = !numbersVisable;
 });
 
 routes.addEventListener("click", (e) => {
-  if (mesh2Visable) {
-    scene.remove(mesh2);
+  if (routesVisable) {
+    scene.remove(OBJroutes);
   } else {
-    scene.add(mesh2);
+    scene.add(OBJroutes);
   }
-  mesh2Visable = !mesh2Visable;
+  routesVisable = !routesVisable;
 });
 
 var animate = function () {
